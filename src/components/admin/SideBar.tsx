73927828cde37183322b2e9c3ff97f254d935e80
sohancwact <sohancwactfw@gmail.com>
2024-03-31 18:50:36 +0545

    configuring tailwind with prettier and some utility classes

--- src/components/admin/SideBar.tsx
+++ src/components/admin/SideBar.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const SideBar = () => {
-  return <div className="border border-white w-[10%]">SideBa</div>;
+  return (
+    <div className="bborder h-[100vh] w-56">
+      <div className="mt-3 p-3 text-center font-bold ">GameStore</div>
+    </div>
+  );
 };
 
 export default SideBar;
